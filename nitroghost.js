4b89b8b8d72e49e2e67a4e539bbd901567275119
NitroGhost <wildeman.benjamin@gmail.com>
2013-05-09 22:00:26 +0200

    Update nitroghost.js

--- nitroghost.js
+++ nitroghost.js
@@ -1,7 +1,7 @@
 delay();
 loadDammit();
 function delay() {
-	setTimeout("load();", 6000);
+  setTimeout("load();", 6000);
 }
 
 function load() {
@@ -39,8 +39,6 @@ function readCookies() {
     	autoqueue = value != null ? value : false;
     	value = jaaulde.utils.cookies.set(COOKIE_STREAM);
     	stream = value != null ? value : true;
-    	value = jaaulde.utils.cookies.get(COOKIE_USERLIST);
-    	userList = value != null ? value : true;
     	value = jaaulde.utils.cookies.get(COOKIE_HIDE_VIDEO);
     	hideVideo = value != null ? value : false;
 	onCookiesLoaded();
@@ -57,12 +55,10 @@ function onCookiesLoaded() {
 		$('#yt-frame').animate({'height': (hideVideo ? '0px' : '271px')}, {duration: 'fast'});
 		$('#playback .frame-background').animate({'opacity': (hideVideo ? '0' : '0.91')}, {duration: 'medium'});
 	}
-	if (userList) {
-		populateUserlist();
-	}
     	initAPIListeners();
     	displayUI();
     	initUIListeners();
+    	populateUserlist();
 }
 
 var words = {
@@ -114,15 +110,9 @@ var skipPassed = 0;
 var timer = null;
 var clickTimer = null;
 var skipTimer = null;
-var autowoot;
-var autoqueue;
-var stream;
-var userList;
-var hideVideo;
 var COOKIE_WOOT = 'autowoot';
 var COOKIE_QUEUE = 'autoqueue';
 var COOKIE_STREAM = 'stream';
-var COOKIE_USERLIST = 'userlist';
 var COOKIE_HIDE_VIDEO = 'hidevideo';
 var MAX_USERS_WAITLIST = 50;
 
@@ -146,7 +136,7 @@ overPlayed = ["1:vZyenjZseXA", "1:ZT4yoZNy90s", "1:Bparw9Jo3dk", "1:KrVC5dm5fFc"
 var styles = [
             '.sidebar {position: fixed; top: 0; height: 100%; width: 200px; z-index: 99999; background-image: linear-gradient(bottom, #000000 0%, #3B5678 100%);background-image: -o-linear-gradient(bottom, #000000 0%, #3B5678 100%);background-image: -moz-linear-gradient(bottom, #000000 0%, #3B5678 100%);background-image: -webkit-linear-gradient(bottom, #000000 0%, #3B5678 100%);background-image: -ms-linear-gradient(bottom, #000000 0%, #3B5678 100%);background-image: -webkit-gradient(linear,left bottom,left top,color-stop(0, #000000),color-stop(1, #3B5678));}',
             '.sidebar#side-right {right: -190px;z-index: 99999;}',
-            '.sidebar#side-left {left: 0px; z-index: 99999; }',
+            '.sidebar#side-left {left: -190px; z-index: 99999; }',
             '.sidebar-handle {width: 12px;height: 100%;z-index: 99999;margin: 0;padding: 0;background: rgb(96, 141, 197);box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, .9);cursor: "ne-resize";}',
             '.sidebar-handle span {display: block;position: absolute;width: 10px;top: 50%;text-align: center;letter-spacing: -1px;color: #000;}',
             '.sidebar-content {position: absolute;width: 185px;height: 100%; padding-left: 15px}',
@@ -212,6 +202,26 @@ var scripts = [
             '                }, 300, "easeOutQuart");',
             '       }, this), 500));',
             '    });',
+            '$("#side-left")',
+            '    .hoverIntent(function() {',
+            '        var timeout_r = $(this)',
+            '            .data("timeout_r");',
+            '        if (timeout_r) {',
+            '            clearTimeout(timeout_r);',
+            '        }',
+            '        $(this)',
+            '            .animate({',
+            '                "left": "0px"',
+            '            }, 300, "easeOutQuart");',
+            '    }, function() {',
+            '        $(this)',
+            '            .data("timeout_r", setTimeout($.proxy(function() {',
+            '            $(this)',
+            '                .animate({',
+            '                    "left": "-190px"',
+            '                }, 300, "easeOutQuart");',
+            '       }, this), 500));',
+            '    });'
 ];
 
 function initAPIListeners() {
@@ -219,20 +229,14 @@ function initAPIListeners() {
   	API.addEventListener(API.CHAT, autoRespond);
   	API.addEventListener(API.DJ_UPDATE, queueUpdate);
   	API.addEventListener(API.VOTE_UPDATE, function (obj) {
-  		if (userList) {
-            		populateUserlist();
-  		}
+            	populateUserlist();
 
     	});
 	API.addEventListener(API.USER_JOIN, function (user) {
-          	if (userList) {
-            		populateUserlist();
-  		}
+          	populateUserlist();
     	});
     	API.addEventListener(API.USER_LEAVE, function (user) {
-            	if (userList) {
-            		populateUserlist();
-  		}
+            	populateUserlist();
     	});
 }
 
@@ -240,13 +244,11 @@ function displayUI() {
 	var colorWoot = autowoot ? '#3FFF00' : '#ED1C24';
     	var colorQueue = autoqueue ? '#3FFF00' : '#ED1C24';
     	var colorStream = stream ? '#3FFF00' : '#ED1C24';
-    	var colorUser = userList ? '#3FFF00' : '#ED1C24';
     	var colorVideo = hideVideo ? '#3FFF00' : '#ED1C24';
 	$('#side-right .sidebar-content').append(
 			'<a id="plug-btn-woot" title="toggles auto woot" style="color:' + colorWoot + '">auto woot</a>'
 		+ 	'<a id="plug-btn-queue" title="toggles auto queue" style="color:' + colorQueue + '">auto queue</a>'
 		+ 	'<a id="plug-btn-stream" title="toggles video stream" style="color:' + colorStream + '">stream</a>'
-		+ 	'<a id="plug-btn-userlist" title="toggles video stream" style="color:' + colorUser + '">userlist</a>'
 		+ 	'<a id="plug-btn-hidevideo" title="toggles hide video" style="color:' + colorVideo + '">hide video</a>'
 		+	'<a id="plug-btn-rules" title="sends rules" style="color:#FF8C00">rules</a>'
 		+	'<a id="plug-btn-face" title="sends fb link" style="color:#FF8C00">like our fb</a>'
@@ -284,25 +286,12 @@ function initUIListeners() {
 	$("#plug-btn-stream").on("click", function() {
 		stream = !stream;
 		$(this).css("color", stream ? "#3FFF00" : "#ED1C24");
-		if (stream) {
+		if (stream == true) {
 			API.sendChat("/stream on");
 		} else { 
 			API.sendChat("/stream off");
 		}
 		jaaulde.utils.cookies.set(COOKIE_STREAM, stream);
-	});
-	$("#plug-btn-userlist").on("click", function() {
-		userList = !userList;
-        	$(this).css('color', userList ? '#3FFF00' : '#ED1C24');
-        	$('.sidebar#side-left').css('visibility', userList ? 'visible' : 'hidden');
-
-        	if (!userList) {
-            		$('.sidebar#side-left').empty();
-        	} 
-		else {
-            		populateUserlist();
-        	}
-        	jaaulde.utils.cookies.set(COOKIE_USERLIST, userList);
 	});
 	$("#plug-btn-hidevideo").on("click", function() {
 		hideVideo = !hideVideo;
@@ -458,9 +447,6 @@ function djAdvanced(obj) {
 		setTimeout("$('#button-vote-positive').click();", 7000);
 	}
 	setTimeout("overPlayedSongs();", 3000);
-	if (userlist) {
-		populateUserList();
-	}
 }
 
 function overPlayedSongs(data) {
@@ -479,7 +465,6 @@ function overPlayedSongs(data) {
 }
 
 function populateUserlist() {
-	$('.sidebar#side-left').html(' ');
 	var mehlist = '';
     	var wootlist = '';
     	var undecidedlist = '';
@@ -574,7 +559,6 @@ function checkSkipped() {
 	}
 }
 
-$('.sidebar#side-left').remove();
 $('#plugbot-css').remove();
 $('#plugbot-js').remove();
 $('#chat-messages').append('<div class="chat-update"><span class="chat-text">Also, welcome to Dubstep, Techno, and Electro custom scipt, coded by Nitro Ghost. Version: 4.1.3</span></div>');
