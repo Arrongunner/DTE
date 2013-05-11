db0887d98b36e1cf132ad47fe42c41b0320f7e1b
NitroGhost <wildeman.benjamin@gmail.com>
2013-05-11 23:09:38 +0200

    Update nitroghost.js

--- nitroghost.js
+++ nitroghost.js
@@ -206,7 +206,7 @@ var scripts = [
             '                    "right": "-190px"',
             '                }, 300, "easeOutQuart");',
             '       }, this), 500));',
-            '    });'
+            '    });',
 ];
 
 function initAPIListeners() {
@@ -288,7 +288,7 @@ function initUIListeners() {
 	$("#plug-btn-userlist").on("click", function() {
 		userList = !userList;
 		$(this).css("color", userList ? "#3FFF00" : "#ED1C24");
-		$(".sidebar#side-left").animate({"left": (userList ? "-190px" : "px")}, 300, "easeOutQuart");
+		$(".sidebar#side-left").animate({"left": (userList ? "-190px" : "0px")}, 300, "easeOutQuart");
 		if (!userList) {
             		$(".sidebar#side-left").empty();
         	} 
