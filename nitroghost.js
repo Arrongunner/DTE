d5c8d393fdd4c01d6b6a551210371d7b844026a2
NitroGhost <wildeman.benjamin@gmail.com>
2013-05-11 23:45:28 +0200

    Update nitroghost.js

--- nitroghost.js
+++ nitroghost.js
@@ -248,8 +248,8 @@ function displayUI() {
 
 function initUIListeners() {
 	$(".sidebar#side-left").on("click", function() {
-		$(this).animate({"left": "0px"}, {duration: "fast"});
 		$(this).animate({"left": "-190px"}, {duration: "fast"});
+		$(this).animate({"left": "0px"}, {duration: "fast"});
 	});
 	$("#plug-btn-woot").on("click", function() {
 		autowoot = !autowoot;
