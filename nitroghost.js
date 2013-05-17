9baf2a16aeecba982491ea73963d8f227005f7e2
NitroGhost <wildeman.benjamin@gmail.com>
2013-05-17 17:12:42 +0200

    Update nitroghost.js

--- nitroghost.js
+++ nitroghost.js
@@ -410,7 +410,7 @@ function overPlayedSongs(data) {
 		API.sendChat("/me auto skip activated! song exceeds 8 minutes long");
 		setTimeout("new RoomPropsService(document.location.href.split('/')[3],true,true,1,5);", 250);
 		setTimeout("new ModerationForceSkipService;", 500);
-		setTimeout("new RoomPropsService(document.location.href.split('/')[3],false,true,1,5);", 7500);
+		setTimeout("new RoomPropsService(document.location.href.split('/')[3],false,true,1,5);", 750);
 	}
 }
 
