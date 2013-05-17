1af8a3b7363b230ad70680b2579c722f81cb7fe8
NitroGhost <wildeman.benjamin@gmail.com>
2013-05-17 17:12:17 +0200

    Update nitroghost.js

--- nitroghost.js
+++ nitroghost.js
@@ -396,21 +396,21 @@ function djAdvanced(obj) {
 	if (autowoot) {
 		setTimeout("$('#button-vote-positive').click();", 7000);
 	}
-	setTimeout("overPlayedSongs();", 5000);
+	setTimeout("overPlayedSongs();", 6000);
 }
 
 function overPlayedSongs(data) {
 	if (overPlayed.indexOf(Models.room.data.media.id) > -1) {
 		API.sendChat("/me auto skip activated! song overplayed");
-		setTimeout("new RoomPropsService(document.location.href.split('/')[3],true,true,1,5);", 1000);
-		setTimeout("new ModerationForceSkipService;", 2000);
-		setTimeout("new RoomPropsService(document.location.href.split('/')[3],false,true,1,5);", 3000);
+		setTimeout("new RoomPropsService(document.location.href.split('/')[3],true,true,1,5);", 250);
+		setTimeout("new ModerationForceSkipService;", 500);
+		setTimeout("new RoomPropsService(document.location.href.split('/')[3],false,true,1,5);", 750);
 	}
 	if (Models.room.data.media.duration > 481) {
 		API.sendChat("/me auto skip activated! song exceeds 8 minutes long");
-		setTimeout("new RoomPropsService(document.location.href.split('/')[3],true,true,1,5);", 1000);
-		setTimeout("new ModerationForceSkipService;", 2000);
-		setTimeout("new RoomPropsService(document.location.href.split('/')[3],false,true,1,5);", 3000);
+		setTimeout("new RoomPropsService(document.location.href.split('/')[3],true,true,1,5);", 250);
+		setTimeout("new ModerationForceSkipService;", 500);
+		setTimeout("new RoomPropsService(document.location.href.split('/')[3],false,true,1,5);", 7500);
 	}
 }
 
