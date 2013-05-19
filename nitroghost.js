7ea51689b1f12b2437aec5f01451809213ccbd74
NitroGhost <wildeman.benjamin@gmail.com>
2013-05-19 19:53:31 +0200

    Update nitroghost.js

--- nitroghost.js
+++ nitroghost.js
@@ -45,16 +45,6 @@ function onCookiesLoaded() {
 	if (left) {
 		$(".sidebar#side-left").animate({"left": left ? "0px" : "-190px"}, 300, "easeOutCirc");
 	}
-	if (emotes) {
-		Emoji.emojify = function(a) {
-			var b=!1;": "==a.substr(0,2)&&(b=!0,a=a.substr(2));for(var c in Emoji._cons)var d=c,e=Emoji._cons[c],d=d.replace("<","&lt;").replace(">","&gt;"),d=RegExp("(\\s|^)("+Emoji._regexEscape(d)+")(?=\\s|$)","g"),a=a.replace(d,"$1:"+e+":");for(c=Emoji._matchStr.exec(a);c;)e=c[1].toLowerCase(),d="&colon;"+e+"&colon;",Emoji._map[e]&&(d='<span class="emoji-glow"><span class="emoji emoji-'+Emoji._map[e]+'"></span></span>'),a=a.substr(0,c.index)+d+a.substr(c.index+c[0].length),c=Emoji._matchStr.exec(a);return(b?": ":"")+a
-		}
-	}
-	if (!emotes) {
-		Emoji.emojify = function(data) {
-			return data;
-		}
-	}
     	initAPIListeners();
     	displayUI();
     	initUIListeners();
@@ -116,6 +106,8 @@ var COOKIE_HIDE_EMOTES = 'emotes';
 var stream = true;
 var COOKIE_LEFT = 'left';
 var MAX_USERS_WAITLIST = 50;
+var emotesOn = function(a) {var b=!1;": "==a.substr(0,2)&&(b=!0,a=a.substr(2));for(var c in Emoji._cons)var d=c,e=Emoji._cons[c],d=d.replace("<","&lt;").replace(">","&gt;"),d=RegExp("(\\s|^)("+Emoji._regexEscape(d)+")(?=\\s|$)","g"),a=a.replace(d,"$1:"+e+":");for(c=Emoji._matchStr.exec(a);c;)e=c[1].toLowerCase(),d="&colon;"+e+"&colon;",Emoji._map[e]&&(d='<span class="emoji-glow"><span class="emoji emoji-'+Emoji._map[e]+'"></span></span>'),a=a.substr(0,c.index)+d+a.substr(c.index+c[0].length),c=Emoji._matchStr.exec(a);return(b?": ":"")+a};
+var emotesOff = function(data) {return data;};
 
 var fbMsg = ["like our facebook page! http://bit.ly/DTandE-FB", "check out our facebook page at http://bit.ly/DTandE-FB", "drop us a like on our facebook page http://bit.ly/DTandE-FB", "like our fb page or die! just kidding http://bit.ly/DTandE-FB"];
 var rulesMsg = "Rules: 1) for all ages so no porn 2) no songs over 8 mins 3) spamming can lead to an instant ban 4) please keep songs to EDM 5) have fun!";
@@ -271,16 +263,7 @@ function initUIListeners() {
 	$("#plug-btn-emotes").on("click", function() {
 		emotes = !emotes;
 		$(this).css("color", emotes ? "#3FFF00" : "#ED1C24");
-		if (emotes) {
-			Emoji.emojify = function(a) {
-				var b=!1;": "==a.substr(0,2)&&(b=!0,a=a.substr(2));for(var c in Emoji._cons)var d=c,e=Emoji._cons[c],d=d.replace("<","&lt;").replace(">","&gt;"),d=RegExp("(\\s|^)("+Emoji._regexEscape(d)+")(?=\\s|$)","g"),a=a.replace(d,"$1:"+e+":");for(c=Emoji._matchStr.exec(a);c;)e=c[1].toLowerCase(),d="&colon;"+e+"&colon;",Emoji._map[e]&&(d='<span class="emoji-glow"><span class="emoji emoji-'+Emoji._map[e]+'"></span></span>'),a=a.substr(0,c.index)+d+a.substr(c.index+c[0].length),c=Emoji._matchStr.exec(a);return(b?": ":"")+a
-			}
-		}
-		if (!emotes) {
-			Emoji.emojify = function(data) {
-				return data;
-			}
-		}
+		Emoji.emojify = emotes ? "emotesOn" : "emotesOff";
 		jaaulde.utils.cookies.set(COOKIE_HIDE_EMOTES, emotes);
 	});
 	$("#plug-btn-face").on("click", function() {
