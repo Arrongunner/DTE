e671dca6769be13d69204af96a4ec97c2002753d
NitroGhost <wildeman.benjamin@gmail.com>
2013-05-19 20:33:36 +0200

    Update nitroghost.js

--- nitroghost.js
+++ nitroghost.js
@@ -24,6 +24,8 @@ function readCookies() {
     	autoqueue = value != null ? value : false;
     	value = jaaulde.utils.cookies.get(COOKIE_HIDE_VIDEO);
     	hideVideo = value != null ? value : false;
+    	var value = jaaulde.utils.cookies.get(COOKIE_EMOTES);
+    	emotes = value != null ? value : true;
     	var value = jaaulde.utils.cookies.get(COOKIE_LEFT);
     	left = value != null ? value : false;
 	onCookiesLoaded();
@@ -42,6 +44,14 @@ function onCookiesLoaded() {
 	}
 	if (left) {
 		$(".sidebar#side-left").animate({"left": left ? "0px" : "-190px"}, 300, "easeOutCirc");
+	}
+	if (emotes) {
+		Emoji.emojify = function(a) {
+			var b=!1;": "==a.substr(0,2)&&(b=!0,a=a.substr(2));for(var c in Emoji._cons)var d=c,e=Emoji._cons[c],d=d.replace("<","&lt;").replace(">","&gt;"),d=RegExp("(\\s|^)("+Emoji._regexEscape(d)+")(?=\\s|$)","g"),a=a.replace(d,"$1:"+e+":");for(c=Emoji._matchStr.exec(a);c;)e=c[1].toLowerCase(),d="&colon;"+e+"&colon;",Emoji._map[e]&&(d='<span class="emoji-glow"><span class="emoji emoji-'+Emoji._map[e]+'"></span></span>'),a=a.substr(0,c.index)+d+a.substr(c.index+c[0].length),c=Emoji._matchStr.exec(a);return(b?": ":"")+a
+		}
+	}
+	if (!emotes) Emoji.emojify = function(data) {
+		return data;
 	}
     	initAPIListeners();
     	displayUI();
@@ -100,10 +110,11 @@ var skipTimer = null;
 var COOKIE_WOOT = 'autowoot';
 var COOKIE_QUEUE = 'autoqueue';
 var COOKIE_HIDE_VIDEO = 'hidevideo';
-var stream = true;
+var COOKIE_EMOTES = 'emotes';
 var COOKIE_LEFT = 'left';
+var stream = true;
 var MAX_USERS_WAITLIST = 50;
-var emotes = true;
+
 
 var fbMsg = ["like our facebook page! http://bit.ly/DTandE-FB", "check out our facebook page at http://bit.ly/DTandE-FB", "drop us a like on our facebook page http://bit.ly/DTandE-FB", "like our fb page or die! just kidding http://bit.ly/DTandE-FB"];
 var rulesMsg = "Rules: 1) for all ages so no porn 2) no songs over 8 mins 3) spamming can lead to an instant ban 4) please keep songs to EDM 5) have fun!";
@@ -267,6 +278,7 @@ function initUIListeners() {
 		if (!emotes) Emoji.emojify = function(data) {
 			return data;
 		}
+		jaaulde.utils.cookies.set(COOKIE_EMOTES, emotes);
 	});
 	$("#plug-btn-face").on("click", function() {
 		if (clicked == false) {
