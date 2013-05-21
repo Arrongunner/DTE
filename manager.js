89826b21c19e1ec7e6c2ecd6be63c80210a80dcc
NitroGhost <wildeman.benjamin@gmail.com>
2013-05-21 20:51:30 +0200

    Update manager.js

--- manager.js
+++ manager.js
@@ -96,15 +96,18 @@ for(var i=0,l=texts.snapshotLength; (this_text=texts.snapshotItem(i)); i++) {
 var mentioned = false;
 var clicked = false;
 var skipped = false;
+var predictor = false;
 var timeToWait = 600000;
 var clickWait = 5000;
 var skipWait = 2000;
 var timePassed = 0;
 var clickPassed = 0;
 var skipPassed = 0;
+var predictPassed = 0;
 var timer = null;
 var clickTimer = null;
 var skipTimer = null;
+var predictTimer = null;
 var COOKIE_WOOT = 'autowoot';
 var COOKIE_QUEUE = 'autoqueue';
 var COOKIE_STREAMING = 'streaming';
@@ -421,6 +424,10 @@ function djAdvanced(obj) {
 	if (autowoot) {
 		$('#button-vote-positive').click();
 	}
+	if (predictor == false) {
+		predictor = true;
+		predictTimer = setInterval("checkPredict()", 1000);
+	}
 }
 
 function populateUserlist() {
@@ -549,6 +556,17 @@ function checkSkipped() {
 	}
 	else {
 		skipPassed = skipPassed + 500;
+	}
+}
+
+function checkPredict() {
+	if (predictPassed >= API.getMedia().duration) {
+		clearInterval(predictTimer);
+		predictor = false;
+		predictPassed = 0;
+	}
+	else {
+		predictPassed = predictPassed + 1;
 	}
 }
 
