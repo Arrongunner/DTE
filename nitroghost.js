d28ac41c90a2a9865c42caf7370d81292be34f45
NitroGhost <wildeman.benjamin@gmail.com>
2013-05-21 11:17:04 +0200

    Update nitroghost.js

--- nitroghost.js
+++ nitroghost.js
@@ -259,7 +259,8 @@ function initUIListeners() {
 		stream = !stream;
 		$(this).css("color", stream ? "#3FFF00" : "#ED1C24");
 		if (stream) {
-			API.sendChat(DB.settings.streamDisabled ? '/stream on' : '/stream off');		
+			API.sendChat(DB.settings.streamDisabled ? '/stream on' : '/stream off');
+		}
 		jaaulde.utils.cookies.set(COOKIE_STREAM, stream);
 	});
 	$("#plug-btn-hidevideo").on("click", function() {
