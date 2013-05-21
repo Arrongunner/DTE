5c075e607a9a772529d1205d2b792c11acb575df
NitroGhost <wildeman.benjamin@gmail.com>
2013-05-21 14:55:33 +0200

    Update nitroghost.js

--- nitroghost.js
+++ nitroghost.js
@@ -519,7 +519,7 @@ function populateUserlist() {
         var spot = Models.room.getWaitListPosition();
         var waitlistDiv = $('<h3 title="waitlist posisition"></h3>').addClass('waitlistspot').text('waitlist: ' + (spot !== null ? spot + ' / ' : '') + Models.room.data.waitList.length);
         var approxtime = Models.room.getWaitListPosition()*240;
-        var timeDiv = $('<h3 title="time to wait"></h3>').addClass('timewait').text('time: ' + (spot !== null ? secondsToString(decodeURIComponent(approxtime)) + ' ' : ''));
+        var timeDiv = $('<h3 title="time to wait"></h3>').addClass('timewait').text('' + (spot !== null ? secondsToString(decodeURIComponent(approxtime)) + ' ' : ''));
         $('#side-left .sidebar-content2').append(waitlistDiv);
         $('#side-left .sidebar-content2').append(spot !== null ? timeDiv : '');
         $('#side-left .sidebar-content2').append('<div class="meanlist"></div>');
@@ -535,7 +535,7 @@ function secondsToString(seconds) {
 	var numhours = Math.floor((seconds % 86400) / 3600);
 	var numminutes = Math.floor(((seconds % 86400) % 3600) / 60);
 	var numseconds = ((seconds % 86400) % 3600) % 60;
-	if(numhours > 0) {
+	if(numhours > 1) {
 		if(numminutes > 9) {
 			return "hours: " + numhours + " mintues: " + nummintues
 		} else {
