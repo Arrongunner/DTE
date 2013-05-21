79267cfcd1889d6712983c3951af29d530aabcac
NitroGhost <wildeman.benjamin@gmail.com>
2013-05-21 13:53:36 +0200

    Update nitroghost.js

--- nitroghost.js
+++ nitroghost.js
@@ -517,7 +517,7 @@ function populateUserlist() {
 	}
         $('#side-left .sidebar-content2').html('<h3 class="users" title="number of users in the room">users: ' + API.getUsers().length + '</h3>');
         var spot = Models.room.getWaitListPosition();
-        var waitlistDiv = $('<h3 title="waitlist posisition"></h3>').addClass('waitlistspot').text('waitlist: ' + (spot !== null ? spot + ' / ' : '') + Models.room.data.waitList.length);
+        var waitlistDiv = $('<h3 title="waitlist posisition"></h3>').addClass('waitlistspot').text('waitlist: ' + (spot !== null ? spot + ' / ' : '') + Models.room.data.waitList.length) + (spot !== null ? ' time: ' + spot*4 : '');
         $('#side-left .sidebar-content2').append(waitlistDiv);
         $('#side-left .sidebar-content2').append('<div class="meanlist"></div>');
         $(".meanlist").append( 
