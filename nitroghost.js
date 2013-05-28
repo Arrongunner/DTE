2d1dd38cec8097b237e7d6a1252c75c97cdef5d4
NitroGhost <wildeman.benjamin@gmail.com>
2013-05-28 02:47:17 +0200

    Update nitroghost.js

--- nitroghost.js
+++ nitroghost.js
@@ -623,15 +623,12 @@ function checkPredict() {
 	}
 }
 
-if (customisedChatCommands !== undefined) {
-    customisedChatCommands.close();
-};
-
+if (meshkaEnhanced !== undefined)
+    meshkaEnhanced.close();
 String.prototype.equalsIgnoreCase = function(other) {
     return this.toLowerCase() === other.toLowerCase();
 };
-
-var customisedChatCommands = Class.extend({
+var meshkaEnhancedModel = Class.extend({
     init: function(){
         ChatModel.chatCommand = function (a) {
             var b;
@@ -673,8 +670,46 @@ var customisedChatCommands = Class.extend({
         Models.chat.chatCommand = ChatModel.chatCommand
         
     },
+    close: function(){
+        ChatModel.chatCommand = function (a) {
+            var b;
+            if ("/help" == a) return a = {
+                    type: "update"
+            }, a.message =
+                Lang.chat.help, this.receive(a), !0;
+            if ("/users" == a) return UserListOverlay.show(), !0;
+            if ("/hd on" == a) return Playback.setHD(!0), !0;
+            if ("/hd off" == a) return Playback.setHD(!1), !0;
+            if ("/chat big" == a) return this.expand(), !0;
+            if ("/chat small" == a) return this.collapse(), !0;
+            if ("/afk" == a) return Models.user.changeStatus(1), !0;
+            if ("/back" == a) return Models.user.changeStatus(0), !0;
+            if (0 == a.indexOf("/ts ")) return b = a.split(" ").pop(), DB.settings.chatTS = "12" == b ? 12 : "24" == b ? 24 : !1, this.dispatchEvent("timestampUpdate", {
+                    value: DB.settings.chatTS
+                }),
+            DB.saveSettings(), !0;
+            if (0 == a.indexOf("/cap ")) {
+                if (a = parseInt(a.split(" ").pop()), 0 < a && 201 > a) return RoomUser.audience.gridData.avatarCap = a, RoomUser.redraw(), DB.settings.avatarcap = a, DB.saveSettings(), log(Lang.messages.cap.split("%COUNT%").join("" + a)), !0
+            } else {
+                if ("/cleanup" == a) return DB.reset(), Dialog.alert(Lang.alerts.updateMessage, $.proxy(Utils.forceRefresh, Utils), Lang.alerts.update, !0), !0;
+                if ("/stream on" == a) DB.settings.streamDisabled = !1, DB.saveSettings(), Playback.media && Playback.play(Playback.media,
+                        Playback.mediaStartTime), b = "Video/audio streaming enabled.";
+                else if ("/stream off" == a) DB.settings.streamDisabled = !0, DB.saveSettings(), Playback.stop(), b = "<strong>Video/audio streaming has been stopped.</strong> Type <em>/stream on</em> to enable again.";
+                else {
+                    if ("/clear" == a) return this.dispatchEvent("chatClear"), _gaq.push(["_trackEvent", "Chat", "Clear", Models.room.data.id]), !0;
+                    Models.room.ambassadors[Models.user.data.id] ? "/fixbooth" == a && (new ModerationBoothCleanupService, b = "Fixing Booth") : Models.room.admins[Models.user.data.id] &&
+                        ("/fixbooth" == a ? (new ModerationBoothCleanupService, b = "Fixing Booth") : 0 == a.indexOf("/audience ") ? (a = parseInt(a.split(" ").pop()), 0 < a ? (RoomUser.testAddAvatar(a), b = "Adding " + a + " fake avatars to audience") : (RoomUser.clear(), RoomUser.setAudience(Models.room.getAudience()), RoomUser.setDJs(Models.room.getDJs()), b = "Cleared fake avatars from audience")) : 0 == a.indexOf("/ping ") ? (DB.settings.showPings = "/ping on" == a ? !0 : !1, DB.saveSettings(), b = "Ping messages are " + (DB.settings.showPings ? "on" : "off")) : 0 == a.indexOf("/speed ") &&
+                        (b = parseInt(a.split(" ").pop()), animSpeed = 0 < b ? b : 83, b = "Setting animation speed to " + animSpeed))
+                }
+            }
+            return b ? (a = {
+                type: "system"
+            }, a.message = b, this.receive(a), !0) : !1
+        }
+        Models.chat.chatCommand = ChatModel.chatCommand
+    }
 });
-var customisedChatCommands = new customisedChatCommandsModel;
+var meshkaEnhanced = new meshkaEnhancedModel;
 
 delay();
 $('#chat-messages').append('<div class="chat-update"><span class="chat-text">Also, welcome to Dubstep, Techno, and Electro custom script, coded by Nitro Ghost. Version: 4.1.3</span></div>');
