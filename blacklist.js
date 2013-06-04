384983feaf3ecde5cf785db41fee487f58798d41
NitroGhost <wildeman.benjamin@gmail.com>
2013-06-04 22:11:39 +0200

    Update blacklist.js

--- blacklist.js
+++ blacklist.js
@@ -1,14 +1,12 @@
-//The blacklist to stop those particularly naughty users joining
-blacklist=
-[ 
-        //"userID"
+blacklist = [ 
+        //user IDs
 ];
 
 API.addEventListener(API.USER_JOIN, checkBlacklist);
 
 function checkBlacklist(user) {
- if(blacklist.indexOf(user.id) > -1 ) {
-     API.sendChat("/me blacklisted user detected, removing them from the room!");
-     API.moderateBanUser(user.id, "Banned from this room")
-   }
- }
+        if (blacklist.indexOf(user.id) > -1 ) {
+                API.sendChat("/me blacklisted user detected, removing them from the room!");
+                API.moderateBanUser(user.id, "Banned from this room");
+        }
+}
