906c9449f8d2dc04c2c3debc659ee9ebee358f57
MyNameIsMidi <james.a.g.middleton@gmail.com>
2013-06-04 22:07:21 +0200

    blacklist.js updated
    Complete and tested - Working.

--- blacklist.js
+++ blacklist.js
@@ -1,1 +1,14 @@
-//future project
+//The blacklist to stop those particularly naughty users joining
+blacklist=
+[ 
+        //"userID"
+];
+
+API.addEventListener(API.USER_JOIN, checkBlacklist);
+
+function checkBlacklist(user) {
+ if(blacklist.indexOf(user.id) > -1 ) {
+     API.sendChat("/me blacklisted user detected, removing them from the room!");
+     API.moderateBanUser(user.id, "Banned from this room")
+   }
+ }
