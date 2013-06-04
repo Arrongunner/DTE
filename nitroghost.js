3c5e7c6145432c4016deba06c38e438ddaf1401e
NitroGhost <wildeman.benjamin@gmail.com>
2013-06-04 20:20:06 +0200

    Update nitroghost.js

--- nitroghost.js
+++ nitroghost.js
@@ -454,21 +454,6 @@ function djAdvanced(obj) {
 	}
 }
 
-function autoSkip(data) {
-	if (overPlayed.indexOf(Models.room.data.media.id) > -1) {
-		API.sendChat("/me auto skip activated! song overplayed");
-		setTimeout("new RoomPropsService(document.location.href.split('/')[3],true,true,1,5);", 250);
-		setTimeout("new ModerationForceSkipService;", 500);
-		setTimeout("new RoomPropsService(document.location.href.split('/')[3],false,true,1,5);", 750);
-	}
-	if (Models.room.data.media.duration > 481) {
-		API.sendChat("/me auto skip activated! song exceeds 8 minutes long");
-		setTimeout("new RoomPropsService(document.location.href.split('/')[3],true,true,1,5);", 250);
-		setTimeout("new ModerationForceSkipService;", 500);
-		setTimeout("new RoomPropsService(document.location.href.split('/')[3],false,true,1,5);", 750);
-	}
-}
-
 function roomSkip(obj) {
 	var tv = obj.negative + obj.positive;
 	var tvp = obj.negative / tv;
