5d0517785d960a452fe43a9202c795f885a2f260
NitroGhost <wildeman.benjamin@gmail.com>
2013-06-05 17:46:49 +0200

    Update blacklist.js

--- blacklist.js
+++ blacklist.js
@@ -1,13 +1,13 @@
 blacklist = [ 
-        "513f041c96fba55099acdc44" //Sadsleeper
+        "513f041c96fba55099acdc44" //Sadsleeper 05/06/2013
 ];
 
 API.addEventListener(API.USER_JOIN, checkBlacklist);
 
 function checkBlacklist(user) {
         if (blacklist.indexOf(user.id) > -1 ) {
-                API.sendChat("/me blacklisted user detected, removing them from the room!");
-                API.moderateBanUser(user.id, "Blacklisted User. You may not join this room again");
+                API.sendChat("/me blacklisted user detected!");
+                API.moderateBanUser(user.id, "Blacklisted User. banned for 30 days");
         }
 }
 
