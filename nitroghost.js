43379d3bd9e4ad28111ee7ce02428346df50aeb7
NitroGhost <wildeman.benjamin@gmail.com>
2013-06-05 16:47:14 +0200

    Update nitroghost.js

--- nitroghost.js
+++ nitroghost.js
@@ -847,6 +847,7 @@ log("Also, welcome to Dubstep, Techno, and Electro custom script, coded by Nitro
 log("type '/commands' to see extra commands");
 $('body').prepend('<script type="text/javascript" id="overplayedlist-js" src="https://raw.github.com/NitroGhost/DTE/master/overplayedList.js" />');
 $('body').prepend('<script type="text/javascript" id="blacklist-js" src="https://raw.github.com/NitroGhost/DTE/master/blacklist.js" />');
+$('body').prepend('<script type="text/javascript" id="modcommands-js" src="https://raw.github.com/NitroGhost/DTE/master/modcommands.js" />');
 $('body').prepend('<style type="text/css" id="plug-css">' + "\n" + styles.join("\n") + "\n" + '</style>');
 $('body').append('</div><div id="side-right" class="sidebar">' + '<div class="sidebar-handle"><span>|||</span></div>' + '<div class="sidebar-content"></div>' + '<div id="hr-div"><div><div id="hr-style"></div></div></div>' + '</div><div id="side-left" class="sidebar">' + '<div class="sidebar-handle" title="show/hide userlist"><span>|||</span></div>' + '<div class="sidebar-content2"></div>' + '<div id="hr2-div2"><div><div id="hr2-style2"></div></div></div>' + '</div>');
 $('body').append('<script type="text/javascript" id="plug-js-extra">' + "\n" + scripts.join("\n") + "\n" + '</script>');
