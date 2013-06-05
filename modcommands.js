e2dc065234c3283a5e7b8805b945c5cf828e490f
NitroGhost <wildeman.benjamin@gmail.com>
2013-06-05 17:31:25 +0200

    Update modcommands.js

--- modcommands.js
+++ modcommands.js
@@ -70,7 +70,7 @@ ccm = Class.extend({
 	customChatCommand: function(a) {
 		var b;
 	 	if ("/commands" == a) {
-	   		return log('<span><strong>Extra Commands:</strong></br>/ca &nbsp; Change Avatar</br>/op &nbsp; Show Track ID</br>/id @username &nbsp; Show User ID</br>/add @username &nbsp; Add User</br>/rm @username &nbsp; Remove User</br>/w @username &nbsp; Send Special Message</br>/strobe off &nbsp; Deactivate Strobes</span>'), !0;
+	   		return log('<span><strong>Extra Commands:</strong></br>/ca &nbsp; Change Avatar</br>/op &nbsp; Show Track ID</br>/id @username &nbsp; Show User ID</br>/add @username &nbsp; Add User To Booth/Waitlist</br>/rm @username &nbsp; Remove User From Booth/Waitlist</br>/w @username &nbsp; Send Special Message</br>/strobe off &nbsp; Deactivate Strobes</span>'), !0;
 	 	}        
 	 	if ("/ca" == a) {
 	   		return Models.user.changeAvatar("halloween" + prompt("Enter Avatar Number:\r\r01 - Male Vampire\r02 - Female Vampire\r03 - Male Frankenstein\r04 - Female Frankenstein\r05 - Male Skeleton\r06 - Female Skeleton\r07 - Male Mummy\r08 - Female Mummy\r09 - Male Ghost\r10 - Male Werewolf\r11 - Pumpkin Man\r12 - Female Werewolf\r13 - Male Zombie", "01")), !0;
